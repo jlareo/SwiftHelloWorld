dc4f43056dcdda0bcd6d858978d50a695429e22e
Jose Antonio Lareo Domínguez <lareomerlin@gmail.com>
2015-07-22 08:13:06 +0100

    Add alert button

--- SwiftHelloWorld/ViewController.swift
+++ SwiftHelloWorld/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func messageButtonPressed(sender: UIButton) {
+
+        let alert = UIAlertController(title: "My message", message: "Hello World!", preferredStyle:UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.Default, handler: nil))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
 
