6ac48313f71ef24ae6bc2006553ff89c40989ae2
jlareo <lareomerlin@gmail.com>
2015-07-22 09:47:05 +0100

    Modify 'created_by' text

--- SwiftHelloWorldTests/SwiftHelloWorldTests.swift
+++ SwiftHelloWorldTests/SwiftHelloWorldTests.swift
@@ -2,7 +2,7 @@
 //  SwiftHelloWorldTests.swift
 //  SwiftHelloWorldTests
 //
-//  Created by Jose Antonio Lareo Domínguez on 22/7/15.
+//  Created by jlareo on 22/7/15.
 //  Copyright (c) 2015 sample. All rights reserved.
 //
 
